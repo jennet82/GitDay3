c066a6135770709f5f56292fecdb6caf3fbf4b82
jennet82 <orazovajennet@gamil.com>
2019-08-07 12:26:11 -0700

    First commit, Login Test1

---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,41 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTests {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void loginTest1() throws Exception{
+     driver.get("http://practice.cybertekschool.com");
+     Thread.sleep(1000);
+     driver.findElement(By.cssSelector("a[href='/checkboxes']")).click();
+     Thread.sleep(1000);
+     driver.findElement(By.cssSelector("[type='checkbox']:nth-of-type(1)")).click();
+     Thread.sleep(1000);
+    }
+
+    @AfterTest
+    public void teardown(){
+        driver.quit();
+    }
+
+}
